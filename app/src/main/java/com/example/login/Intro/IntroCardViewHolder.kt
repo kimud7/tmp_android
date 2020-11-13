b0f64708cfb6820b10f957f8d0fce480a4642a32
kimud7 <kimud7@gmail.com>
2020-11-13 15:40:51 +0900

    imageSlide

--- app/src/main/java/com/example/login/Intro/IntroCardViewHolder.kt
+++ app/src/main/java/com/example/login/Intro/IntroCardViewHolder.kt
@@ -1,7 +1,9 @@
 package com.example.login.Intro
 
+import android.util.Log
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.item_container.view.*
 
 class IntroCardViewHolder(v:View):RecyclerView.ViewHolder(v) {
@@ -10,5 +12,6 @@ class IntroCardViewHolder(v:View):RecyclerView.ViewHolder(v) {
     fun bind(item: IntroCard){
         view.Txt_Intro_Card_Title.text = item.title
         view.Txt_Intro_Card_Content.text = item.content
+        Picasso.get().load(item.imageurl).into(view.kbv_item)
     }
 }
--- app/src/main/java/com/example/login/IntroActivity.kt
+++ app/src/main/java/com/example/login/IntroActivity.kt
@@ -5,12 +5,16 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.login.Intro.IntroCard
 import com.example.login.Intro.IntroCardAdapter
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_intro.*
+import kotlinx.android.synthetic.main.item_container.*
+import kotlinx.android.synthetic.main.item_container.view.*
 
 class IntroActivity:AppCompatActivity(){
     val IntroCardList : List<IntroCard> = listOf(
-            IntroCard("kim","undong","https://cdn.pixabay.com/photo/2015/09/09/17/38/basil-932079_960_720.jpg") ,
-            IntroCard("test","test2","test3")
+            IntroCard("테스트입니다","이것은 테스트입니다","https://cdn.pixabay.com/photo/2015/09/09/17/38/basil-932079_960_720.jpg") ,
+            IntroCard("테스트입니다2","이것은 테스트입니다2","https://cdn.pixabay.com/photo/2015/09/09/17/38/basil-932079_960_720.jpg"),
+            IntroCard("테스트입니다3","이것은 테스트입니다3","https://cdn.pixabay.com/photo/2015/09/09/17/38/basil-932079_960_720.jpg")
 
     )
     override fun onCreate(savedInstanceState: Bundle?) {
